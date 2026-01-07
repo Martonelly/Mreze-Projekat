d09b749ef7c694b6214613ddc645a01d4722dac0
Marton Viktor Melar <melar.pr9.2023@uns.ac.rs>
2026-01-07 22:14:26 +0100

    Reorganize using directives and comments in Program.cs
    Ftn

--- Mreze Projekat/Mreze Projekat/Program.cs
+++ Mreze Projekat/Mreze Projekat/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//FTN
 //Test
 //Marton PR 9/2023
 //Denis PR 14/2023
@@ -17,3 +18,4 @@ namespace Mreze_Projekat
         }
     }
 }
+
